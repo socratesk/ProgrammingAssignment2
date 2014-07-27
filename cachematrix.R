d542f82f55d0da4d849a39fea37e9c01fe79b89f
socratesk <socratesk@gmail.com>
2014-07-27 12:59:11 -0500

    Code comments added for each operation

--- cachematrix.R
+++ cachematrix.R
@@ -14,25 +14,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	## variable initialisation for inverse property
 	a <- NULL
 
+	## operation to set the matrix
 	set <- function (matrx) {
 		x <<- matrx
 		a <<- NULL
 	}
 
+	## operation to get the matrix
 	get <- function() {
 		x
 	}
 
+	## operation to set the inverse of the matrix
 	setInverse <- function (invrse) {
 		a <<- invrse
 	}
 
+	## operation to get the inverse of the matrix
 	getInverse <-  function() {
 		a
 	}
 
+	## returns list of all the operations in this function
 	list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
@@ -43,17 +49,25 @@ makeCacheMatrix <- function(x = matrix()) {
 ### If NO, it computes inverse value and sets the same in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
 
+	## outputs matrix that is inverse of x matrix
 	inv <- x$getInverse()
 
+	## Verifies if the inverse exits. If Yes, returns the same
 	if (!is.null(inv)) {
 		return (inv)
 	}
 
+	## gets the matrix from object and stores it in data
 	data <- x$get()
+
+	## computes the inverse matrix using matrix multiplication
 	inv <- solve(data) %% data
+	
+	## Inverse matrix is being set in the object
 	x$setInverse(inv)
 
+	## Finally returns the inverse matrix
 	inv
 }
